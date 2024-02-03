e939a78ee3172e9a9b1eec5a280cb47e0d94f351
Nimish Karn <150058194+Nimish-Codes@users.noreply.github.com>
2024-02-03 13:58:23 +0530

    Update user_engagement_prediction.py

--- user_engagement_prediction.py
+++ user_engagement_prediction.py
@@ -11,7 +11,7 @@ def predict_engagement(selected_features):
     if engagement_score > 20:
         return st.success("Well done! It's user engaging.")
     else:
-        st.failure("Not much engaging need to improve.")
+        st.write("Not much engaging need to improve.")
 
 st.title("Digital Marketing Engagement Predictor")
 
